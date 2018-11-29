4a865a01caa96e4671c2912fa429c8e309975ade
Abdo23 <abdou0230@gmail.com>
2018-11-29 21:02:32 +0200

    Base folders+ Main

--- main.lua
+++ main.lua
@@ -5,3 +5,10 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+display.setStatusBar( display.HiddenStatusBar )
+display.setDefault( "background", 1,1,1 )
+
+screen = require ( "Utils.screen")
+math2 = require ("Utils.math2")
+color = require ("Utils.color")
